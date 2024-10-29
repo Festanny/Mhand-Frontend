4c79bdffac23466dbfe9272361266bdf4884a5eb
Festanny <60522731+Festanny@users.noreply.github.com>
2024-10-29 15:18:41 +0400

    update

--- src/scripts/modules/slider.js
+++ src/scripts/modules/slider.js
@@ -4,4 +4,13 @@ export function slider() {
           el: ".swiper-pagination",
         },
     });
+
+    new Swiper(".bannSlider", {
+      slidesPerView: 1.7,
+      spaceBetween: 21,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
 }
